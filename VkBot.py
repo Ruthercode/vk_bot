973167cefd3ab85c3fb2877d7a91522273f7ff69
ruthercode <ruthercode@gmail.com>
2020-02-03 21:21:53 +0300

    fix message send

--- VkBot.py
+++ VkBot.py
@@ -1,5 +1,6 @@
 import vk_api
 import time
+import random
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
@@ -13,9 +14,9 @@ class VkBot:
         self.token = token
         self.session = vk_api.VkApi(token=token)
         self.vk = self.session.get_api()
-        self.commands = {"лайкай": self.likes_add_for_person}
+        self.commands = {"лайкай": self.likes_from_bot}
 
-    def likes_add_for_person(self, targets, album):
+    def likes_from_bot(self, targets, album,count=1000):
         code = 0
         targets = self.vk.users.get(user_ids=targets)
         for target in targets:
@@ -33,7 +34,7 @@ class VkBot:
                 photos = self.vk.photos.get(owner_id=target['id'],
                                             album_id=album,
                                             rev=1,
-                                            count=1000)
+                                            count=count)
                 photos = photos["items"]
             except vk_api.exceptions.ApiError:
                 code = 1
@@ -55,27 +56,37 @@ class VkBot:
             return code
 
     def say(self, message, identificator, chat):
+        random.seed()
+        random_number = random.randint(10000,100000)
         if chat:
             self.vk.messages.send(chat_id=identificator,
-                                  message=message)
+                                  message=message,
+                                  random_id= random_number)
         else:
             self.vk.messages.send(user_id=identificator,
-                                  message=message)
+                                  message=message,
+                                  random_id= random_number)
 
     def start_longpoll(self):
         longpoll = VkLongPoll(self.session)
+
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
                 message = event.text.split()
+
                 if message[0] in self.commands.keys():
+
                     command = message[0]
+                    count = message[-2]
                     mood = message[-1]
-                    targets = message[1:-1]
-                    code = self.commands[command](targets, mood)
+                    targets = message[1:-2]
+                    code = self.commands[command](targets, mood,count)
+
                     if code:
                         message = "Failed"
                     else:
                         message = "Success"
+
                     if event.from_user:  # Если написали в ЛС
                         self.say(message, event.user_id, 0)
                     elif event.from_chat:  # Если написали в Беседе
