99303c5c20cfbbaa70cedbd43c07b2fd87466a7c
ruthercode <ruthercode@gmail.com>
2020-02-04 20:34:23 +0300

    add "help" command

--- vkbot.py
+++ vkbot.py
@@ -15,9 +15,9 @@ class VkBot:
     def __init__(self, token):
         self.token = token
         self.vk = vk_api.VkApi(token=token).get_api()
-        self.commands = {"погода": self.__get_weather}  # TODO: add new commands and fix old
+        self.commands = {"погода": self.__get_weather,
+                         "помощь": self.__help}  # TODO: add new commands and fix old
 
-    # TODO: add "help" command
     def likes_from_bot(self, target_ids, album, count=1000):
         """Bot send POST request for VK API (Method likes.add)
         for all user's photos received through the photos.get method
@@ -25,8 +25,7 @@ class VkBot:
         :param targets: list of id's
         :param album: - string
         :param count: - int <= 1000
-        :returns 0 or 1: """
-        code = 0
+        :returns None: """
         targets = self.vk.users.get(user_ids=target_ids)
 
         for target in targets:
@@ -36,7 +35,6 @@ class VkBot:
                 if is_closed:
                     raise ClosedPageException
             except ClosedPageException:
-                code = 1
                 print("{0}'s page ( https://vk.com/{1} ) is closed. Bot has no access".format(target["first_name"],
                                                                                               target["id"]))
                 continue
@@ -48,7 +46,6 @@ class VkBot:
                                             count=count)
                 photos = photos["items"]
             except vk_api.exceptions.ApiError:
-                code = 1
                 print("{0}'s ( https://vk.com/{1} ) album is closed. Bot has no access".format(target["first_name"],
                                                                                                target["id"]))
                 continue
@@ -60,13 +57,11 @@ class VkBot:
                                       item_id=photo['id'])
                 except self.vk.api.exceptions.ApiError:
                     print("Error")
-                    return 1
                 else:
                     print("Photo (id: {0}) was liked".format(photo['id']))
 
                 time.sleep(2)
 
-            return code
 
     def send_message(self, message, send_id):
         """Send POST request for VK API (messages.send)
@@ -77,7 +72,7 @@ class VkBot:
                               message=message,
                               random_id=random_number)
 
-    def __get_weather(self,message):
+    def __get_weather(self, message):
         sity = "moscow"
         country = "ru"
         if message.__len__() > 0:
@@ -95,7 +90,12 @@ class VkBot:
 
         resp = requests.get(url=url, params=params)
         d_weather = resp.json()
+
         print(d_weather)
+
+        if d_weather['cod'] == '404':
+            return "Город не найден"
+
         weather = {'description': d_weather['weather'][0]['description'],
                    'temp': floor(d_weather['main']['temp']),
                    'feels_like': floor(d_weather['main']['feels_like']),
@@ -105,30 +105,43 @@ class VkBot:
 
         wind_vector = ""
         if 180 > weather['wind_deg'] > 0:
-            wind_vector = wind_vector + "c"
+            wind_vector = wind_vector + "С"
         elif 360 > weather['wind_deg'] > 180:
-            wind_vector = wind_vector + "ю"
+            wind_vector = wind_vector + "Ю"
 
         if 270 > weather['wind_deg'] > 90:
-            wind_vector = wind_vector + "з"
+            wind_vector = wind_vector + "З"
         elif weather['wind_deg'] != 90 and weather['wind_deg'] != 270:
-            wind_vector = wind_vector + "в"
+            wind_vector = wind_vector + "В"
         weather['wind_deg'] = wind_vector
 
-        return "В городе {sity} сейчас {description} \n" \
-               "Температура: {temp}\n"\
-               "Ощущается как {feels_like} \n"\
-               "Ветер {wind_deg}, cкорость {wind_speed}мс  \n"\
-                "Облачность: {clouds}%".format(sity=d_weather['name']+', '+d_weather['sys']['country'],
-                                               description=weather['description'],
-                                               temp=weather['temp'],
-                                               feels_like=weather['feels_like'],
-                                               wind_deg=weather['wind_deg'],
-                                               wind_speed=weather['wind_speed'],
-                                               clouds=weather['clouds'])
+        return 'В городе {sity} сейчас {description} \n' \
+               'Температура: {temp}\n' \
+               'Ощущается как {feels_like} \n' \
+               'Ветер {wind_deg}, cкорость {wind_speed}мс  \n' \
+               'Облачность: {clouds}%'.format(sity=d_weather['name'] + ', ' + d_weather['sys']['country'],
+                                              description=weather['description'],
+                                              temp=weather['temp'],
+                                              feels_like=weather['feels_like'],
+                                              wind_deg=weather['wind_deg'],
+                                              wind_speed=weather['wind_speed'],
+                                              clouds=weather['clouds'])
 
-    def __command_handler(self,event):
-        message = event.text.lower().translate(str.maketrans("","",".,?!")).split()
+    def __help(self, message):
+        commands_description = {
+            "Погода %город%": "Выдаёт информацию о текущей погоде. Можно указать страну"}
+        # TODO: Переодически обновлять
+
+        response = "Все команды начинаются с обращения Эрнест или Эрнесто. \n" \
+                   "Список команд: \n"
+
+        for key in commands_description.keys():
+            response = response + key + ' - ' + commands_description[key] + '\n'
+
+        return response
+
+    def __command_handler(self, event):
+        message = event.text.lower().translate(str.maketrans("", "", ".,?!")).split()
 
         call = message[0]
         if call.__len__() < 6:
@@ -137,14 +150,15 @@ class VkBot:
             return
 
         message = message[1:]
-        if message[0] in self.commands.keys():
+        if message.__len__() == 0:
+            response = "Да да я"
+        elif message[0] in self.commands.keys():
             response = self.commands[message[0]](message[1:])
         else:
-            response = "Команда не распознана, просьба совершить передвижение в сторону мужских половых органов"
-
+            response = "Команда не распознана, используйте команду 'помощь', чтобы узнать список команд"
 
         if event.from_user:
-            self.send_message(response,event.user_id)
+            self.send_message(response, event.user_id)
         elif event.from_chat:
             self.send_message(response, 2000000000 + event.chat_id)
 
@@ -154,4 +168,3 @@ class VkBot:
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
                 self.__command_handler(event)
-
