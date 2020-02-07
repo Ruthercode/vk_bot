f85f267662cae6d9805828b15bd7cf476dcca66a
ruthercode <ruthercode@gmail.com>
2020-02-07 15:59:02 +0300

    fix date bug

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.vkbot import VkBot
 
-token = "5277d48df2317b0597d43fa36fb0b694bc50f6496a516cb4f62ec4e26098a4efd157a155b01230cc46645"  # Bot's token.
+token = "5277d48df2317b0597d43fa36fb0b694bc50f6496a516cb4f62ec4e26098a4efd157a155b01230cc46635"  # Bot's token.
 bot = VkBot(token=token)
 
 while True:
